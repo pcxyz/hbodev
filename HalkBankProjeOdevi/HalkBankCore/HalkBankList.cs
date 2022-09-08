ca32456a6824197902c5dcd3265d4e994de94504
CA4OKDG <CA4OKDG@NB340905C1039>
2022-09-09 02:29:59 +0300

    odev

---
+++ HalkBankProjeOdevi/HalkBankCore/HalkBankList.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalkBankCore
+{
+    public class HalkBankList : IBankList, IEnumerable
+    {
+        private object[] _dizi;
+
+        public HalkBankList()
+        {
+            _dizi = new object[0];
+        }
+
+        public object this[int index]
+        {
+            get => _dizi[index];
+            set => _dizi[index] = value;
+        }
+
+        public int ElemanSayisi => _dizi.Length;
+
+        public void Ekle(object eleman)
+        {
+            var yedekDizi = _dizi;
+            _dizi = new object[ElemanSayisi + 1];
+
+            yedekDizi.CopyTo(_dizi, 0);
+
+            _dizi[ElemanSayisi-1] = eleman;
+        }
+
+    
+        public void KosulluSil(Func<object, bool> kosul)
+        {
+            int count = 0;
+            for (int i = _dizi.Length - 1; i >= 0; i--)
+            {
+                if (kosul(_dizi[i]))
+                {
+                    ++count;
+                  Sil(i);
+                }
+            }
+       
+        }
+        public IEnumerator GetEnumerator()
+        {
+          return  _dizi.Cast<string>().GetEnumerator();
+        }
+
+        public void Sil(int index)
+        {
+            var yedekDizi = _dizi;
+            _dizi = new object[ElemanSayisi - 1];
+
+            int sayac = 0;
+
+            for (int i = 0; i < yedekDizi.Length; i++)
+            {
+                if (i == index)
+                    continue;
+
+                _dizi[sayac] = yedekDizi[i];
+
+                sayac++;
+            }
+        }
+
+        public void Temizle()
+        {
+            _dizi = new object[0];
+        }
+    }
+}
---
+++ HalkBankProjeOdevi/HalkBankListDemo1/Program.cs
@@ -0,0 +1,42 @@
+﻿using HalkBankCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalkBankListDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new HalkBankList();
+            list.Ekle("Salih");
+            list.Ekle("Engin");
+            list.Ekle("Mustafa");
+            list.Ekle("Ahmet");
+
+            list[2] = "Mehmet";
+
+            list.Sil(3);
+
+            Console.WriteLine("Eleman Sayısı:{0}", list.ElemanSayisi);
+
+   
+            foreach (var eleman in list)
+            {
+                Console.WriteLine(eleman);
+            }
+            list.KosulluSil(isim => isim == "Mehmet");
+
+            foreach (var eleman in list)
+            {
+                Console.WriteLine(eleman);
+            }
+            Console.WriteLine("Eleman Sayısı:{0}", list.ElemanSayisi);
+
+            Console.ReadLine();
+        }
+    }
+}
